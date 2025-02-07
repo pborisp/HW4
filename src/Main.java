ae3fe2932c4b64adae7b952c61e5ef4f27381df5
Slava ASUS <popovbs@gmail.com>
2025-02-07 21:35:18 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,20 @@ public class Main {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
         System.out.println();
+
+        // Задача №2
+        System.out.println("Задача №2:");
+        int clientDeviceYear = 2015;
+        clientOS = 1;
+        if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        System.out.println();
     }
 }
