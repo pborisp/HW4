beefc404be560e4173b4db045e95cbc36c499433
Slava ASUS <popovbs@gmail.com>
2025-02-07 21:21:49 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        // Задача №1
+        System.out.println("Задача №1:");
+        byte clientOS = 0;
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        System.out.println();
     }
 }
